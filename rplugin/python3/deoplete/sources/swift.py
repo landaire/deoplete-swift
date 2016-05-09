f6e49bce46d7702857f97cbb2dd6a43d96be0839
Tomoya Kose <tomoya@mitsuse.jp>
2016-05-10 00:24:08 +0900

    Find the binary of SourceKitten by using `shutil.which`.

--- rplugin/python3/deoplete/sources/swift.py
+++ rplugin/python3/deoplete/sources/swift.py
@@ -1,5 +1,6 @@
 import os
 import re
+import shutil
 import subprocess
 import sys
 import json
@@ -127,18 +128,8 @@ class Source(Base):
             return self.find_binary_path('sourcekitten')
 
     def find_binary_path(self, cmd):
-        def is_exec(fpath):
-            return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+        path = shutil.which(cmd, mode=os.X_OK)
+        if path is None:
+            return error(self.vim, cmd + ' binary not found')
 
-        fpath, fname = os.path.split(cmd)
-        if fpath:
-            if is_exec(cmd):
-                return cmd
-        else:
-            for path in os.environ["PATH"].split(os.pathsep):
-                path = path.strip('"')
-                binary = os.path.join(path, cmd)
-                if is_exec(binary):
-                    return binary
-        return error(self.vim, cmd + ' binary not found')
-
+        return path
